6dcc3e4529f4e87925cb17a58799af8f6e7b8f26
gwendalaubert <gwendal.aubert@akeneo.com>
2023-01-31 18:27:54 +0100

    ODX-7: Minor changes prior to the dockerfiles

--- samples/start-app/js-node-express/server.js
+++ samples/start-app/js-node-express/server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
 
-app.set('port', process.env.PORT || 8082);
+app.set('port', process.env.PORT || 80);
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 8082);
+server.listen(process.env.PORT || 80);
