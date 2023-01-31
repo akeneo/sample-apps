40bcf93a9470f200fdcfe94cb7ec83809319a641
gwendalaubert <gwendal.aubert@akeneo.com>
2023-01-31 18:43:24 +0100

    ODX-7: Changing server starter script

--- samples/start-app/js-node-express/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('./app');
-
-require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
-
-app.set('port', process.env.PORT || 80);
-const server = http.createServer(app);
-
-server.listen(process.env.PORT || 80);
