d6734cd77e8d39b77c62deb93267472c8e93d510
Samuel Gomis <samuel.gomis@getakeneo.com>
2023-02-07 17:10:51 +0100

    feat(ODX-11): add html templates

--- samples/start-app/python-fastapi/app/controller/callback.py
+++ samples/start-app/python-fastapi/app/controller/callback.py
@@ -2,12 +2,22 @@ from fastapi import APIRouter, Request, Depends
 from sqlalchemy.orm import Session
 from ..dependencies import get_db, get_session
 from ..usecase.callback import callback_usecase
+from fastapi.responses import HTMLResponse
+from ..persistence import tokenRepository
+from pathlib import Path
+
 
 
 router = APIRouter()
 
 @router.get("/callback")
 def callback(request: Request, db: Session = Depends(get_db), session: object = Depends(get_session)):
-    response = callback_usecase(request, db, session)
+    callback_usecase(request, db, session)
 
-    return response.json()
+    token = tokenRepository.get_token(db)
+
+    if(token is None):
+            return HTMLResponse(content=Path('app/templates/no_access_token.html').read_text(), status_code=200);
+
+
+    return HTMLResponse(content=Path('app/templates/access_token.html').read_text(), status_code=200)
--- samples/start-app/python-fastapi/app/main.py
+++ samples/start-app/python-fastapi/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Depends
 from .persistence.database import engine
-from .controller import activate, callback, firstApiCall
+from .controller import activate, callback, firstApiCall, homepage
 from .persistence import models
 from .dependencies import get_session
 
@@ -11,4 +11,5 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(activate.router, dependencies=[Depends(get_session)])
 app.include_router(callback.router, dependencies=[Depends(get_session)])
 app.include_router(firstApiCall.router)
+app.include_router(homepage.router)
 
