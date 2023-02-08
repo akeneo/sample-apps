9275a71a6f009ed4199ca89b1afddbdd63321041
Samuel Gomis <gomis.samuel@gmail.com>
2023-02-08 17:46:57 +0100

    Update samples/start-app/python-fastapi/app/persistence/tokenRepository.py
    Co-authored-by: gwendalaubert <gwendal.aubert@akeneo.com>

--- samples/start-app/python-fastapi/app/persistence/tokenRepository.py
+++ samples/start-app/python-fastapi/app/persistence/tokenRepository.py
@@ -10,7 +10,7 @@ def create_token(db: Session, token: schemas.TokenCreate):
     token_from_db = get_token(db);
     db_token = models.Token(access_token=token.access_token)
     if token_from_db is not None and token_from_db.access_token == token.access_token:
-        return db_token;
+        return db_token
 
     db.add(db_token)
     db.commit()
