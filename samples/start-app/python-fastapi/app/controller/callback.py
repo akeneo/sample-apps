34f3316c8428a233bb7874707f5121b89e0f42e7
Samuel Gomis <gomis.samuel@gmail.com>
2023-02-08 16:49:11 +0100

    Update samples/start-app/python-fastapi/app/controller/callback.py
    Co-authored-by: gwendalaubert <gwendal.aubert@akeneo.com>

--- samples/start-app/python-fastapi/app/controller/callback.py
+++ samples/start-app/python-fastapi/app/controller/callback.py
@@ -17,7 +17,7 @@ def callback(request: Request, db: Session = Depends(get_db), session: object = 
     token = tokenRepository.get_token(db)
 
     if(token is None):
-            return HTMLResponse(content=Path('app/templates/no_access_token.html').read_text(), status_code=200);
+            return HTMLResponse(content=Path('app/templates/no_access_token.html').read_text(), status_code=200)
 
 
     return HTMLResponse(content=Path('app/templates/access_token.html').read_text(), status_code=200)
