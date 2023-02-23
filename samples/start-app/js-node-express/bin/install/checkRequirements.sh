092a274227fba89f37b343c9033dfec91437e0ec
Samuel Gomis <samuel.gomis@getakeneo.com>
2023-02-23 17:08:27 +0100

    feat(ODX-183): update makefile and check requirements to use docker compose V2

--- samples/start-app/js-node-express/bin/install/checkRequirements.sh
+++ samples/start-app/js-node-express/bin/install/checkRequirements.sh
@@ -19,23 +19,21 @@ if ! [ -x "$(command -v docker)" ]; then
     exit 1
 fi
 
-docker-compose version > /dev/null 2>&1
+docker compose version > /dev/null 2>&1
 DOCKER_COMPOSE_STATUS=$?
 
 if ! [ $DOCKER_COMPOSE_STATUS -eq 0 ]; then
     echo -e $(printf "${WARNING}Docker Compose is mandatory in order to run your apps.${ENDCOLOR}")
-    echo -e $(printf "${WARNING}Please install the latest version of docker-compose on your computer before running this script.${ENDCOLOR}")
+    echo -e $(printf "${WARNING}Please install the latest version of docker compose on your computer before running this script.${ENDCOLOR}")
     echo -e $(printf "${WARNING}Install documentation : https://docs.docker.com/compose/install/${ENDCOLOR}")
     exit 1
 fi
 
-SERVER_VERSION=$(docker-compose version --short)
-SERVER_VERSION_MINOR=$(echo "$SERVER_VERSION"| cut -d'.' -f 2)
-SERVER_VERSION_BUILD=$(echo "$SERVER_VERSION"| cut -d'.' -f 3)
+SERVER_VERSION=$(docker compose version --short)
+SERVER_VERSION_MAJOR=$(echo "$SERVER_VERSION"| cut -d'.' -f 1)
 
-if [ "${SERVER_VERSION_MINOR}" -lt 27 ] || \
-   [[ "${SERVER_VERSION_MINOR}" -eq 27  && "${SERVER_VERSION_BUILD}" -lt 1 ]]  ; then
-    echo -e $(printf "${WARNING}docker-compose version should be 1.27.1 or higher(${WARNING}current version is ${SERVER_VERSION})${ENDCOLOR}.")
+if [ "${SERVER_VERSION_MAJOR}" -eq 1 ]; then
+    echo -e $(printf "${WARNING}Docker Compose V1 is deprecated, you should use Docker Compose V2 (your current version is ${SERVER_VERSION})${ENDCOLOR}.")
     exit 1
 fi
 
