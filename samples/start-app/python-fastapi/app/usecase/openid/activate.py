ad6e719766328b11fcf0ca71ba1ad06b6e9cbda6
Samuel Gomis <samuel.gomis@getakeneo.com>
2023-03-30 14:48:56 +0200

    feat(ODX-210): fix

--- samples/start-app/python-fastapi/app/usecase/openid/activate.py
+++ samples/start-app/python-fastapi/app/usecase/openid/activate.py
@@ -4,7 +4,7 @@ oauth_scopes_openid = oauth_scopes + [
     'openid',
     'email',
     'profile'
-];
+]
 
 def activate_usecase_with_openid(request, session):
     return activate_usecase(request,session, oauth_scopes_openid)
--- samples/start-app/python-fastapi/app/usecase/openid/callback.py
+++ samples/start-app/python-fastapi/app/usecase/openid/callback.py
@@ -5,9 +5,12 @@ from app.usecase.callback import callback_usecase
 from app.utils.codec import encoder, decoder
 from urllib.parse import urljoin
 import requests
-import jwt
+from cryptography.x509 import load_pem_x509_certificate
 from jwt import decode, get_unverified_header
 from jwt.algorithms import get_default_algorithms
+
+
+from cryptography.hazmat.backends import default_backend
 
 
 openid_public_key = '/connect/apps/v1/openid/public-key'
@@ -55,18 +58,18 @@ def fetch_openid_public_key(pim_url):
 def extract_claims_from_signed_token(id_token: str, signature: str, issuer: str):
     algorithm = 'RS256'  # assuming the algorithm used is RSA with SHA-256
     jwt_header = get_unverified_header(id_token)
-    jwt_payload = decode(id_token, signature, algorithms=[algorithm], options={"verify_signature": False})
+    jwt_payload = decode(id_token, algorithms=[algorithm], options={"verify_signature": False})
 
     # Verify the issuer
     if jwt_payload.get('iss') != issuer:
         raise ValueError('Invalid issuer')
 
-    # Verify the signature
-    # algorithms = get_default_algorithms()
-    # signer = algorithms[algorithm]
-    # print(signature)
-    # key = signer.prepare_key(signature)
-    # if not signer.verify(id_token.encode('utf-8'), key, jwt_header):
+    # # Verify the signature
+    # pem_cert = signature.encode('utf-8')
+    # cert = load_pem_x509_certificate(pem_cert, default_backend())
+    # public_key = cert.public_key()
+
+    # if not signer.verify(id_token.encode('utf-8'), public_key, jwt_header):
     #     raise ValueError('Invalid signature')
 
     return jwt_payload
