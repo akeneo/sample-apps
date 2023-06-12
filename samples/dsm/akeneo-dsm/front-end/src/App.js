6aff6fa37536205c99a0bf6dcccb8cc2f2a7bdf2
Quentin Mallet <quentin.mallet@akeneo.com>
2023-06-12 17:09:33 +0200

    feat(ODX-283): corrections

--- samples/dsm/akeneo-dsm/front-end/src/App.js
+++ samples/dsm/akeneo-dsm/front-end/src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import {useState, useEffect} from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
