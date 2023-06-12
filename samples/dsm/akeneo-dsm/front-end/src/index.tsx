50ecbb2d60d953d8500f851385d3a9119ec602c6
Quentin Mallet <quentin.mallet@akeneo.com>
2023-06-12 11:41:05 +0200

    feat(ODX-284): downgrade to react 16.14 to be able to install DSM

--- samples/dsm/akeneo-dsm/front-end/src/index.tsx
+++ samples/dsm/akeneo-dsm/front-end/src/index.tsx
@@ -8,8 +8,8 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'));
-
+  document.getElementById('root') as HTMLElement
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
