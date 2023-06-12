780dad123dcc57bbf9e39a5eb697b2ee6c19be4e
Quentin Mallet <quentin.mallet@akeneo.com>
2023-06-12 15:10:33 +0200

    feat(ODX-284): add DSM and display 'Hello World!" badge

--- samples/dsm/akeneo-dsm/front-end/src/App.tsx
+++ samples/dsm/akeneo-dsm/front-end/src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ThemeProvider } from 'styled-components';
+import { Badge, connectorTheme as theme } from 'akeneo-design-system';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+      <div>
+        <Badge level="primary">Hello Word!</Badge>
+      </div>
+      </ThemeProvider>
+    </>
   );
 }
 
