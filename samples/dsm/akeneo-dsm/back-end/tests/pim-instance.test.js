5d465aa4b1455a974ea31a313e6f5719b9abd7e1
Quentin Mallet <quentin.mallet@akeneo.com>
2023-06-14 15:44:58 +0200

    feat(ODX-288): add test for pim controller

---
+++ samples/dsm/akeneo-dsm/back-end/tests/pim-instance.test.js
@@ -0,0 +1,13 @@
+const {instance} = require("../controllers/pim.controller");
+
+test("It sends a json with the pim instance", async () => {
+    const req = {};
+    const res = { json: {},
+        json: function(input) { this.json = input } 
+    };
+    const next = jest.fn();
+
+    instance(req, res, next);
+
+    expect(res.json).toEqual({'pim-instance': process.env.AKENEO_PIM_URL});
+});
