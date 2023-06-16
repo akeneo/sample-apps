8037bbeff822d199a82516b246ea2676de33d2ab
Quentin Mallet <quentin.mallet@akeneo.com>
2023-06-16 11:14:51 +0200

    feat(ODX-288): update routing

--- samples/dsm/akeneo-dsm/back-end/routes/api.js
+++ samples/dsm/akeneo-dsm/back-end/routes/api.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const pimController = require("../controllers/api/pim.controller.js");
 
 const router = express.Router();
 
-
+router.get('/my-pim-instance', pimController.instance);
 
 module.exports = router;
--- samples/dsm/akeneo-dsm/back-end/routes/index.js
+++ samples/dsm/akeneo-dsm/back-end/routes/index.js
@@ -2,14 +2,11 @@ const express = require('express');
 const appActivation = require('./app-activation.js');
 const api = require('./api.js');
 const homepageController = require("../controllers/homepage.controller.js");
-const pimController = require("../controllers/pim.controller.js");
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', homepageController.index);
-
-router.get('/my-pim-instance', pimController.instance);
 
 router.use(appActivation);
 router.use(api);
