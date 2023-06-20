58cc1deea13d754fa33fd465b07edc47f6ea28c6
Quentin Mallet <quentin.mallet@akeneo.com>
2023-06-20 11:33:47 +0200

    feat(ODX-285): get object from the pim instance

--- samples/dsm/akeneo-dsm/back-end/use-cases/api/pim-instance.js
+++ samples/dsm/akeneo-dsm/back-end/use-cases/api/pim-instance.js
@@ -1,6 +1,30 @@
-const pim_instance = function(req, res, next) {
+const pimInstance = function(req, res, next) {
     const myPimInstance = process.env.AKENEO_PIM_URL;
     res.json({'pim-instance': myPimInstance});
 }
 
-module.exports = { pim_instance };
+let doPimProductsRetrieve = function({
+    httpsClient,
+    tokenDb
+}) {
+    return async function pimProductsRetrieve(req, res, next) {
+        if (!await tokenDb.hasToken()) {
+            res.json({'no access token': 'Missing access token in database'});
+        } else {
+            const token = await tokenDb.getToken();
+
+            httpsClient.setToken(token);
+    
+            const options = {
+                path: '/api/rest/v1/products?limit=100',
+                method: 'GET'
+            };
+    
+            const productsFromPim = await httpsClient.request(options);
+    
+            res.json({products: productsFromPim._embedded.items});
+        }
+    }
+}
+
+module.exports = { pimInstance, doPimProductsRetrieve };
