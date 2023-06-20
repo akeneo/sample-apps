bd8b2e62acc0a90f4ba0ee401a3010748bedac67
Quentin Mallet <quentin.mallet@akeneo.com>
2023-06-20 11:35:42 +0200

    feat(ODX-285): rename and add new handler for the new route

--- samples/dsm/akeneo-dsm/back-end/controllers/api/pim.controller.js
+++ samples/dsm/akeneo-dsm/back-end/controllers/api/pim.controller.js
@@ -1,5 +1,7 @@
-const { pim_instance } = require('../../use-cases/api/pim-instance');
+const { pimInstance } = require('../../use-cases/api/pim-instance');
+const { pimProductsRetrieve } = require('../../use-cases/api');
 
 module.exports = {
-    instance: (req, res, next) => pim_instance(req, res, next)
+    instance: (req, res, next) => pimInstance(req, res, next),
+    productsRetrieve: (req, res, next) => pimProductsRetrieve(req, res, next)
 }
