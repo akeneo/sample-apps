a062de203e5bbeda694e5da46f0221c8c5ba4f6e
Quentin Mallet <quentin.mallet@akeneo.com>
2023-06-21 16:24:55 +0200

    feat(ODX-285): update import

--- samples/dsm/akeneo-dsm/back-end/use-cases/api/index.js
+++ samples/dsm/akeneo-dsm/back-end/use-cases/api/index.js
@@ -1,6 +1,6 @@
 const { httpsClient } = require('../../client/index');
 const { tokenDb } = require('../../data-access/index');
-const { doPimProductsRetrieve } = require('./pim-instance');
+const { doPimProductsRetrieve } = require('./productRetrieve');
 
 const pimProductsRetrieve = doPimProductsRetrieve({
     httpsClient,
