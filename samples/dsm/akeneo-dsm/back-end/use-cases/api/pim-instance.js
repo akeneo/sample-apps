8057e8019b9711d27396ddceef191043d1590fe2
Quentin Mallet <quentin.mallet@akeneo.com>
2023-06-21 16:24:38 +0200

    feat(ODX-285): export the product retrieving to a new use-case

--- samples/dsm/akeneo-dsm/back-end/use-cases/api/pim-instance.js
+++ samples/dsm/akeneo-dsm/back-end/use-cases/api/pim-instance.js
@@ -3,44 +3,4 @@ const pimInstance = function(req, res, next) {
     res.json({'pim-instance': myPimInstance});
 }
 
-let doPimProductsRetrieve = function({
-    httpsClient,
-    tokenDb
-}) {
-    return async function pimProductsRetrieve(req, res, next) {
-        if (!await tokenDb.hasToken()) {
-            res.json({'no access token': 'Missing access token in database'});
-        } else {
-            const token = await tokenDb.getToken();
-
-            httpsClient.setToken(token);
-    
-            const options = {
-                path: '/api/rest/v1/products?limit=100',
-                method: 'GET'
-            };
-    
-            const productsFromPim = await httpsClient.request(options);
-
-            const formattedProducts = formatProducts(productsFromPim._embedded.items);
-    
-            res.json({products: formattedProducts});
-        }
-    }
-
-    function formatProducts(products) {
-        return products.map(product => {
-            const formattedProduct = {}
-
-            formattedProduct.uuid = product.uuid;
-            formattedProduct.identifier = product.identifier;
-            formattedProduct.enabled = product.enabled;
-            formattedProduct.family = product.family;
-            formattedProduct.categories = product.categories;
-
-            return formattedProduct;
-        });
-    }
-}
-
-module.exports = { pimInstance, doPimProductsRetrieve };
+module.exports = { pimInstance };
