6fec0d6279cf23f54d9a12ee66350b5862707937
Quentin Mallet <quentin.mallet@akeneo.com>
2023-06-26 17:31:25 +0200

    DX(107): create header with logo and title

--- samples/dsm/akeneo-dsm/front-end/src/App.tsx
+++ samples/dsm/akeneo-dsm/front-end/src/App.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
+import './App.css';
+import { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { Badge, connectorTheme as theme } from 'akeneo-design-system';
+import logo from "./logo-my-custom-start-app.svg";
+import { Badge, 
+        connectorTheme as theme,
+        Breadcrumb } from 'akeneo-design-system';
 
 function App() {
+  const [pimInstance, setPimInstance] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8081/my-pim-instance', { mode: 'cors' })
+        .then((res) => res.json())
+        .then((data) => {
+            setPimInstance(JSON.stringify(data));
+        })
+        .catch((err) => {
+            console.log('error : ' + err);
+        });
+  }, []);
+
   return (
     <>
       <ThemeProvider theme={theme}>
-      <div>
-        <Badge level="primary">Hello Word!</Badge>
-      </div>
+        <img className='app-logo' src={logo} alt="akeneo custom app logo" />
+        <div className='breadcrumb'>
+          <Breadcrumb>
+            <Breadcrumb.Step>
+              PRODUCT LIST
+            </Breadcrumb.Step>
+            <Breadcrumb.Step href={pimInstance ?? '#'}>
+              {pimInstance ? JSON.parse(pimInstance)['pim-instance'] : 'MY PIM INSTANCE'}
+            </Breadcrumb.Step>
+          </Breadcrumb>
+        </div>
+
+        <h2 className='dashboardTitle'>Dashboard</h2>
       </ThemeProvider>
     </>
   );
