f34c899609f811b41f2bdef835b606241ed353d7
Quentin Mallet <quentin.mallet@akeneo.com>
2023-06-28 15:44:47 +0200

    feat(DX107): improve indentation

--- samples/dsm/akeneo-dsm/front-end/src/component/ProductsTable.tsx
+++ samples/dsm/akeneo-dsm/front-end/src/component/ProductsTable.tsx
@@ -30,80 +30,80 @@ function ProductsTable({ products }: { products: Product[]}) {
 
     return (
         <>
-        <ThemeProvider theme={theme}>
-            <section>
-            <img className='app-logo' src={logo} alt="akeneo custom app logo" />
-            <div className='breadcrumb'>
-                <Breadcrumb>
-                <Breadcrumb.Step>
-                    PRODUCT LIST
-                </Breadcrumb.Step>
-                <Breadcrumb.Step href={pimInstance ?? '#'}>
-                    {pimInstance ? JSON.parse(pimInstance)['pim-instance'] : 'MY PIM INSTANCE'}
-                </Breadcrumb.Step>
-                </Breadcrumb>
-            </div>
+            <ThemeProvider theme={theme}>
+                <section>
+                <img className='app-logo' src={logo} alt="akeneo custom app logo" />
+                <div className='breadcrumb'>
+                    <Breadcrumb>
+                    <Breadcrumb.Step>
+                        PRODUCT LIST
+                    </Breadcrumb.Step>
+                    <Breadcrumb.Step href={pimInstance ?? '#'}>
+                        {pimInstance ? JSON.parse(pimInstance)['pim-instance'] : 'MY PIM INSTANCE'}
+                    </Breadcrumb.Step>
+                    </Breadcrumb>
+                </div>
 
-            <h2 className='dashboardTitle'>Dashboard</h2>
-            </section>
-            
+                <h2 className='dashboardTitle'>Dashboard</h2>
+                </section>
+                
 
-            <section className='products-table'>
-            
-            <Table>
-                <Table.Header sticky={0}>
-                <Table.HeaderCell>
-                    Uuid
-                </Table.HeaderCell>
-                <Table.HeaderCell>
-                    Identifier
-                </Table.HeaderCell>
-                <Table.HeaderCell>
-                    Family
-                </Table.HeaderCell>
-                <Table.HeaderCell>
-                    Categories
-                </Table.HeaderCell>
-                <Table.HeaderCell>
-                    Status
-                </Table.HeaderCell>
-                </Table.Header>
-                <Table.Body>
-                {products.map(product => (
-                    <Table.Row>
-                    <Table.Cell>
-                        {product.uuid}
-                    </Table.Cell>
-                    <Table.Cell>
-                        {product.identifier}
-                    </Table.Cell>
-                    <Table.Cell>
-                        {product.family}
-                    </Table.Cell>
-                    <Table.Cell>
-                        <ul>
-                            {product.categories.map(category => (
-                                <li>{category}</li>
-                            ))}
-                        </ul>
-                    </Table.Cell>
-                    <Table.Cell>
-                        {
-                        product.enabled ? (
-                        <Badge level="primary">
-                            ENABLED
-                        </Badge> ) : (
-                        <Badge level="warning">
-                            WARNING
-                        </Badge> )
-                        }
-                    </Table.Cell>
-                    </Table.Row>
-                ))}
-                </Table.Body>
-            </Table>
-            </section>
-        </ThemeProvider>
+                <section className='products-table'>
+                
+                <Table>
+                    <Table.Header sticky={0}>
+                        <Table.HeaderCell>
+                            Uuid
+                        </Table.HeaderCell>
+                        <Table.HeaderCell>
+                            Identifier
+                        </Table.HeaderCell>
+                        <Table.HeaderCell>
+                            Family
+                        </Table.HeaderCell>
+                        <Table.HeaderCell>
+                            Categories
+                        </Table.HeaderCell>
+                        <Table.HeaderCell>
+                            Status
+                        </Table.HeaderCell>
+                    </Table.Header>
+                    <Table.Body>
+                        {products.map(product => (
+                            <Table.Row>
+                            <Table.Cell>
+                                {product.uuid}
+                            </Table.Cell>
+                            <Table.Cell>
+                                {product.identifier}
+                            </Table.Cell>
+                            <Table.Cell>
+                                {product.family}
+                            </Table.Cell>
+                            <Table.Cell>
+                                <ul>
+                                    {product.categories.map(category => (
+                                        <li>{category}</li>
+                                    ))}
+                                </ul>
+                            </Table.Cell>
+                            <Table.Cell>
+                                {
+                                    product.enabled ? (
+                                    <Badge level="primary">
+                                        ENABLED
+                                    </Badge> ) : (
+                                    <Badge level="warning">
+                                        WARNING
+                                    </Badge> )
+                                }
+                            </Table.Cell>
+                            </Table.Row>
+                        ))}
+                    </Table.Body>
+                </Table>
+                </section>
+            </ThemeProvider>
         </>
     );
 }
