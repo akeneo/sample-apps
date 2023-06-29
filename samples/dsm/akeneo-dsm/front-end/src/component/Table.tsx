5fe7d6db0e32dc7d70e790868feeaf6b5a0e9121
Quentin Mallet <quentin.mallet@akeneo.com>
2023-06-29 12:03:23 +0200

    feat(DX107): create new component to display products

---
+++ samples/dsm/akeneo-dsm/front-end/src/component/Table.tsx
@@ -0,0 +1,71 @@
+import { Badge,
+    Table } from 'akeneo-design-system';
+
+interface Product {
+    uuid: string;
+    identifier: string;
+    family: string;
+    categories: string[];
+    enabled: boolean;
+}
+
+function ProductsTable({ products }: { products: Product[]}) {
+    return (
+        <section className='products-table'>
+            <Table>
+                <Table.Header sticky={0}>
+                    <Table.HeaderCell>
+                        Uuid
+                    </Table.HeaderCell>
+                    <Table.HeaderCell>
+                        Identifier
+                    </Table.HeaderCell>
+                    <Table.HeaderCell>
+                        Family
+                    </Table.HeaderCell>
+                    <Table.HeaderCell>
+                        Categories
+                    </Table.HeaderCell>
+                    <Table.HeaderCell>
+                        Status
+                    </Table.HeaderCell>
+                </Table.Header>
+                <Table.Body>
+                    {products.map(product => (
+                        <Table.Row>
+                        <Table.Cell>
+                            {product.uuid}
+                        </Table.Cell>
+                        <Table.Cell>
+                            {product.identifier}
+                        </Table.Cell>
+                        <Table.Cell>
+                            {product.family}
+                        </Table.Cell>
+                        <Table.Cell>
+                            <ul>
+                                {product.categories.map(category => (
+                                    <li>{category}</li>
+                                ))}
+                            </ul>
+                        </Table.Cell>
+                        <Table.Cell>
+                            {
+                                product.enabled ? (
+                                <Badge level="primary">
+                                    ENABLED
+                                </Badge> ) : (
+                                <Badge level="warning">
+                                    WARNING
+                                </Badge> )
+                            }
+                        </Table.Cell>
+                        </Table.Row>
+                    ))}
+                </Table.Body>
+            </Table>
+        </section>
+    )
+}
+
+export default ProductsTable;
