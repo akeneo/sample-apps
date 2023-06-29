ba11d91fa2be402f8faa4c09ceedcf940c90a6f4
Quentin Mallet <quentin.mallet@akeneo.com>
2023-06-29 14:22:33 +0200

    feat(DX107): delete response param

--- samples/dsm/akeneo-dsm/back-end/app.js
+++ samples/dsm/akeneo-dsm/back-end/app.js
@@ -39,7 +39,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.set('Access-Control-Allow-Origin', '*');
 
   // render the error page
   res.status(err.status || 500);
