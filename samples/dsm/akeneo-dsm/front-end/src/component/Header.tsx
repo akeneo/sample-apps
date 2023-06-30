e5f758b52c1adcd38cf88e524d91c338a4727577
Quentin Mallet <quentin.mallet@akeneo.com>
2023-06-30 16:12:04 +0200

    feat(DX107): update the ternary condition to display the pim version

--- samples/dsm/akeneo-dsm/front-end/src/component/Header.tsx
+++ samples/dsm/akeneo-dsm/front-end/src/component/Header.tsx
@@ -7,14 +7,22 @@ function Header({ pimInstance }: { pimInstance: string | null}) {
             <img className='app-logo' src={logo} alt="akeneo custom app logo" />
             <div className='breadcrumb'>
                 <Breadcrumb>
-                <Breadcrumb.Step>
-                    PRODUCT LIST
-                </Breadcrumb.Step>
-                <Breadcrumb.Step>
-                    <a href={pimInstance ? JSON.parse(pimInstance)['pim-instance'] : '#'}>
-                        {pimInstance ? JSON.parse(pimInstance)['pim-instance'].split('//')[1] : 'MY PIM INSTANCE'}
-                    </a>
-                </Breadcrumb.Step>
+                    <Breadcrumb.Step>
+                        PRODUCT LIST
+                    </Breadcrumb.Step>
+                    <Breadcrumb.Step>
+                        {
+                            pimInstance ? (
+                                <a href={JSON.parse(pimInstance)['pim-instance']}>
+                                    {JSON.parse(pimInstance)['pim-instance'].split('//')[1]}
+                                </a>
+                            ) : (
+                                <a>
+                                    MY PIM INSTANCE
+                                </a>
+                            )
+                        }
+                    </Breadcrumb.Step>
                 </Breadcrumb>
             </div>
             <h2 className='dashboardTitle'>Dashboard</h2>
