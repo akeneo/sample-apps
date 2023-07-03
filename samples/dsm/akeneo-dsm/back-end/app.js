a032dbc2209eb10d4d82e9ee4cf7915f133c9ea9
Quentin Mallet <quentin.mallet@akeneo.com>
2023-07-03 15:45:48 +0200

    feat(DX107): update CORS management

--- samples/dsm/akeneo-dsm/back-end/app.js
+++ samples/dsm/akeneo-dsm/back-end/app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   next();
 });
 
