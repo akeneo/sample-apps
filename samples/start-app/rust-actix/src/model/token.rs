fe122114c5a709144f5f8950686483a3b87a36b0
Fabien D <fabien@myprod.net>
2023-08-27 18:00:50 +0200

    Implement repositories

--- samples/start-app/rust-actix/src/model/token.rs
+++ samples/start-app/rust-actix/src/model/token.rs
@@ -1,4 +1,39 @@
+use anyhow::{anyhow, Result};
+use rusqlite::Connection;
+
 struct Token {
     id: i32,
     access_token: String,    
+}
+
+trait TokenRepository {
+    fn save(&self, conn: Connection, token: Token) -> Result<()>;
+    fn find_by_access_token(&self, conn: Connection, access_token: &str) -> Result<Token>;
+}
+
+impl TokenRepository for Token {
+    fn save(&self, conn: Connection, token: Token) -> Result<()> {
+        conn.execute(
+            "INSERT INTO token (access_token) VALUES (?)",
+            [token.access_token],
+        ).unwrap();
+
+        Ok(())
+    }
+
+    fn find_by_access_token(&self, conn: Connection, access_token: &str) -> Result<Token> {
+        let mut stmt = conn.prepare("SELECT * FROM token WHERE access_token = ?").unwrap();
+        let token_iter = stmt.query_map([access_token], |row| {
+            Ok(Token {
+                id: row.get(0)?,
+                access_token: row.get(1)?,
+            })
+        }).unwrap();
+
+        for token in token_iter {
+            return Ok(token.unwrap());
+        }
+
+        Err(anyhow!("Token not found"))
+    }
 }
--- samples/start-app/rust-actix/src/model/user.rs
+++ samples/start-app/rust-actix/src/model/user.rs
@@ -1,7 +1,49 @@
+use anyhow::{anyhow, Result};
+use rusqlite::Connection;
+
 struct User {
     id: i32,
     username: String,
     password: String,
     email: String,
     sub: String,
+}
+
+
+trait UserRepository {
+    fn save(&self, conn: Connection, user: User) -> Result<()>;
+    fn find_by_sub(&self, conn: Connection, sub: i32) -> Result<User>;
+}
+
+
+impl UserRepository for User {
+    fn save(&self, conn: Connection, user: User) -> Result<()> {
+        
+        conn.execute(
+            "INSERT INTO user (username, password, email, sub) VALUES (?, ?, ?, ?)",
+            [user.username, user.password, user.email, user.sub],
+        )?;
+
+        Ok(())
+    }
+
+    fn find_by_sub(&self, conn: Connection, sub: i32) -> Result<User> {
+        
+        let mut stmt = conn.prepare("SELECT * FROM user WHERE sub = ?")?;
+        let user_iter = stmt.query_map([sub], |row| {
+            Ok(User {
+                id: row.get(0)?,
+                username: row.get(1)?,
+                password: row.get(2)?,
+                email: row.get(3)?,
+                sub: row.get(4)?,
+            })
+        })?;
+
+        for user in user_iter {
+            return Ok(user?);
+        }
+
+        Err(anyhow!("User not found"))
+    }
 }
